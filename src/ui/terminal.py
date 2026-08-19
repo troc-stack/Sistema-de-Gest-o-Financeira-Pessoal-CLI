07120c1e336c8689c9b39a445d8df8087544723b
troc-stack <trocades.jow@gmail.com>
2026-08-19 17:45:48 -0300

    Create terminal.py

---
+++ src/ui/terminal.py
@@ -0,0 +1,17 @@
+def menu():
+    print('--' * 20)
+    print('Controle Finaceiro')
+    print('--' * 20)
+    print('1. Adicionar receita\n'
+        '2. Adicionar despesa\n'
+        '3. Listar transações\n'
+        '4. Editar transação\n'
+        '5. Excluir transação\n'
+        '6. Buscar transação\n'
+        '7. Relatório financeiro\n'
+        '8. Exportar dados\n'
+        '9. Sair')
+    print('--' * 20)
+    return input('Responda uma das opções entre 1 ao 9')
+
+    
