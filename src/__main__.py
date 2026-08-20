5e4a3573403d50558753ae0feb79000fd37a9fc9
troc-stack <trocades.jow@gmail.com>
2026-08-20 19:02:26 -0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -33,14 +33,26 @@ def main():
             gg = GerenciadorTransacoes.Gerenciador
             gg.listar
 
-
-
         elif resp == 4:
-            print('Editar transação')
+            gg = GerenciadorTransacoes.Gerenciador
+            gg.listar
+
+            id = input('informe o ID da trnasação que deseja editar')
+            gg.buscar 
+            
+
+
+
         elif resp == 5: 
             print('Excluir transação')
+
         elif resp == 6: 
             print('Buscar transação')
+            gg = GerenciadorTransacoes.Gerenciador
+
+            id = input('informe o ID da trnasação que deseja editar')
+            gg.buscar
+
         elif resp == 7:
             print('Relatório financeiro')
         elif resp == 8: 
