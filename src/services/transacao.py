8e1455a62551c6bdf367a4aa6ada5dc3f2f7695b
troc-stack <trocades.jow@gmail.com>
2026-08-20 10:19:06 -0300

    Update transacao.py

--- src/services/transacao.py
+++ src/services/transacao.py
@@ -1,17 +1,27 @@
+import json
 
 class Transacoes:
 
-    def __init__(self, Id, Tipo, descricao, valor, categoria, data):
+    def __init__(self, Id, Tipo, valor, categoria, data):
         self.__Id = Id 
         self.__Tipo = Tipo 
-        self.__descrição = descricao
         self.__valor = valor 
         self.__categoria = categoria
         self.__data = data 
 
-    def salvar_arquivo():
-        try: 
-            with open("transacoes.json", "r", encoding="utf-8") as arquivo: 
+    def salvar_arquivo(self):
+        nova_transacao = {
+            { 'id': self.__Id, 'tipo': self.__Tipo, 'valor': self.__valor, 'categoria' : self.__categoria, 'data': self.__data}
+            }
+
+        try:
+            with open("transacoes.json", "r", encoding="utf-8") as arquivo:
                 dados = json.load(arquivo)
-        except FileNotFoundError:
-            print('arquivo não encontrado!')
+        except (FileNotFoundError, json.JSONDecodeError):
+            dados = {"transações": []}
+
+        dados["transações"].append(nova_transacao)
+
+   
+        with open("transacoes.json", "w", encoding="utf-8") as arquivo:
+            json.dump(dados, arquivo, ensure_ascii=False, indent=4)
