6d2319de530d41bb20eaa7daca56e7393289e691
troc-stack <trocades.jow@gmail.com>
2026-08-20 09:39:55 -0300

    Create transacao.py

---
+++ src/services/transacao.py
@@ -0,0 +1,14 @@
+
+class Transacoes:
+
+    def __init__(self, Id, Tipo, descricao, valor, categoria, data):
+        self.__Id = Id 
+        self.__Tipo = Tipo 
+        self.__descrição = descricao
+        self.__valor = valor 
+        self.__categoria = categoria
+        self.__data = data 
+
+    def salvar_arquivo():
+        try: 
+            
